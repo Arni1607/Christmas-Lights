9c5b1312b5542be29e8f05b605035118e4cae235
Arni Tiwari <arni027btcse25@igdtuw.ac.in>
2026-03-22 19:28:56 +0530

    Added interactiveness to buttons through Javascript

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // javascript 
+
+const circle = document.getElementsByClassName(circle)
+const play = document.getElementById(on)
+const stop = document.getElementById(off)
+var len = circle.length;
+
+const on = function on(){
+    for(var i =0; i<len; i++){
+        circle[i].removeAttribute("style")
+        circle[i].style.animationPlayState = "running";
+    }
+}
+
+const off = function off(){
+        title.style.animation = "none";
+        for(var i=0; i<len; i++){
+            circle[i].style.animation = "none";
+            circle[i].style.background="rgb(79, 20, 99)";
+        }
+}
+
+play.addEventListener('click', on);
+stop.addEventListener('click', off);
